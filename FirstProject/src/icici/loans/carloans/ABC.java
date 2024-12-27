0fa74823ab258534946f79784e0936b705708b06
sai vishwanath Chilukuri <saivishwanath11596@gmail.com>
2024-12-27 07:51:41 -0500

    Commit changes

---
+++ FirstProject/src/icici/loans/carloans/ABC.java
@@ -0,0 +1,12 @@
+package icici.loans.carloans;
+
+public class ABC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloworld");
+		System.out.println("Hi Sai");
+
+	}
+
+}
